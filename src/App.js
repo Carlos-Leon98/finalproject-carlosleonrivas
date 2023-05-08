a73dcb42685befefde271ad31f43000cf4104fda
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2023-05-07 19:59:22 -0500

    First manual commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Carlos Leon Rivas - This is my final project
         </p>
         <a
           className="App-link"
